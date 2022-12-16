38f40bc7751913a47654a0e03e2bf5b170dc3452
XinyuP <xinyup@umich.edu>
2022-12-16 17:41:39 -0500

    add static assets(public/index,about,help.html), serve up the static assets, delete the old serve up in app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,55 @@
+const path = require('path')
 const express = require('express');
 // express is just a function
 // call it to create a new express application
 
+// console.log(__dirname) // C:\Users\pangx\Desktop\NodeTutorial\web-server\src
+// // console.log(__filename) // C:\Users\pangx\Desktop\NodeTutorial\web-server\src\app.js
+// console.log(path.join(__dirname, '../public'))
+
 const app = express()
 
-app.get('', (req, res) => { // incoming request to the server, response: contains a bunch of methods allowing us to customize what we are going to send back to the requester z
-    // look at req to figure out what we wanna do
-    // like read data from database, create HTML 
+// configure exoress to serve the directory up
 
-    // use various methods on res to send a response back 
-    res.send('<h1>Weather</h1>') // this allow us to send something back to the requester 
-    // display on browser
-})
-// this let us configure what the server should do when someone try to get the resource at a specific URL
-// takes two argument
-// first is the route(the partial URL)
-// second is a function to describe what we wanna do when someone visit that particular route
+
+app.use(express.static(path.join(__dirname, '../public')))
+// app.use() is a way to customize your server
+// express.static(), we pass its return value into app.use()
+// static() take the path to the folder we wanna serve up
 
 
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Blaire',
-        age: 21
-    }, {
-        name: 'Sara',
-    }])
-})
+// app.get('', (req, res) => { // incoming request to the server, response: contains a bunch of methods allowing us to customize what we are going to send back to the requester z
+//     // look at req to figure out what we wanna do
+//     // like read data from database, create HTML
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
+//     // use various methods on res to send a response back
+//     res.send('<h1>Weather</h1>') // this allow us to send something back to the requester
+//     // display on browser
+// })
+// // this let us configure what the server should do when someone try to get the resource at a specific URL
+// // takes two argument
+// // first is the route(the partial URL)
+// // second is a function to describe what we wanna do when someone visit that particular route
+
+
+
+// // app.use(express.static(path.join(__dirname, '../public/help')))
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Blaire',
+//         age: 21
+//     }, {
+//         name: 'Sara',
+//     }])
+// })
+
+
+
+// // app.use(express.static(path.join(__dirname, '../public/about')))
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -41,7 +60,6 @@ app.get('/weather', (req, res) => {
 
 // in reality, either send back HTML designed to be rendered in the browser,
 // or send back JSON object designed to be used by code
-
 
 
 
