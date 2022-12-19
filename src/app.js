1f65dff98d9cfb96f24ff87053da42115a09c691
XinyuP <xinyup@umich.edu>
2022-12-19 00:09:23 -0500

    add forecast.js and geocode.js and build a json http endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 
 // express is just a function
@@ -98,17 +100,31 @@ app.get('/help', (req, res) => {
 //     res.send('<h1>About</h1>')
 // })
 
+
+// endpoint 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
             error: 'You must provide an address!'
         })
     }
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Ann Arbor',
-        address: req.query.address
-    })
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => { // need to add default
+        if (error) {
+            return res.send(error);
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send(error);
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        });
+    });
+
 })
 
 
